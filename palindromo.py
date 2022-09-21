813ebf52330820d7e15cc181ef687c9e576c2479
guribe1378 <giovannyuribe@gmail.com>
2022-09-21 10:39:04 -0500

    Add files via upload

---
+++ palindromo.py
@@ -0,0 +1,18 @@
+"""
+Palíndromo
+es una palabra que se lee igual en un sentido que en otro
+se utiliza una lista y sus propiedades extend y reverse
+"""
+def palindromo(palabra):
+    palabra=input("Ingresa una palabra: ")
+    lista=[]
+    lista.extend(palabra)
+    lista.reverse()
+    strpalabra=''.join(lista)
+    if palabra==strpalabra:
+        print("La palabra " + "'" + palabra +"'"+  " es palíndromo")
+    else:
+        print("La palabra " + "'" + palabra +"'"+ " no es palíndromo")
+
+    
+palindromo("palabra") 
